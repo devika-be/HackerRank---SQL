c07a1c7e9c790c971a61d4635929aa480e0a003f
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2021-04-21 11:18:41 +0530

    Create Revising Aggregations - Averages.sql

---
+++ Revising Aggregations - Averages.sql
@@ -0,0 +1,7 @@
+#Problem link : https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+
+#Ans :
+
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
