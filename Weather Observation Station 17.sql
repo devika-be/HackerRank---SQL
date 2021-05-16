d189371aae73103aac61c24a3ed966a5cb783585
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2021-05-16 23:24:53 +0530

    Create Weather Observation Station 17.sql

---
+++ Weather Observation Station 17.sql
@@ -0,0 +1,11 @@
+#Problem Link : https://www.hackerrank.com/challenges/weather-observation-station-17/problem
+
+#Ans :
+
+SELECT *
+FROM
+  ( SELECT ROUND(LONG_W, 4)
+   FROM STATION
+   WHERE LAT_N > 38.7780
+   ORDER BY LAT_N ASC)
+WHERE ROWNUM = 1;
