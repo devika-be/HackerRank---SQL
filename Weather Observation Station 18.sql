3676fb62490ee34120125259499aced7d514f604
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2021-05-18 22:20:58 +0530

    Create Weather Observation Station 18.sql

---
+++ Weather Observation Station 18.sql
@@ -0,0 +1,6 @@
+#Problem Link : https://www.hackerrank.com/challenges/weather-observation-station-18/problem
+
+#Ans :
+
+SELECT ROUND(MAX(LAT_N) - MIN(LAT_N) + MAX(LONG_W) - MIN(LONG_W), 4)
+FROM STATION;
